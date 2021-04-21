b969777376fd4663274d98622ba848715afd3ac9
swareham-booth <swareham@chicagobooth.edu>
2021-04-21 19:42:52 +0000

    Aquarius 100%

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   
   get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
   get("/zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
   # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
   # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
